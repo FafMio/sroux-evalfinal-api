932fc56224b947ecf5837041c46eb7ce9dda7629
Sylvain Roux <fafmio@outlook.fr>
2021-01-29 18:53:19 +0100

    Update Serie.php

--- src/Entity/Serie.php
+++ src/Entity/Serie.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
@@ -54,6 +55,7 @@ class Serie
     private $poster;
 
     /**
+     * @ApiSubresource()
      * @ORM\ManyToMany(targetEntity=Comment::class, inversedBy="series")
      */
     private $comments;
